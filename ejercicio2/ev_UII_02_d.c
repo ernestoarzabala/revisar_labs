8ed09992798eebc2a3831ae5fd1e8d1c10ffbdca
ernestoarzabala <61065150+ernestoarzabala@users.noreply.github.com>
2021-11-23 18:59:04 -0700

    update 23/11/2021
    update 23/11/2021

--- ejercicio2/ev_UII_02_d.c
+++ ejercicio2/ev_UII_02_d.c
@@ -4,24 +4,9 @@
 
 int main(void)
 {
-    char letra;
-    int numero_entero;
-    double numero_decimal;
-
-    string cadena_de_caracteres ;
+    printf("Evidencia\tII.2\tCompilación y ejecución de programas sencillos desarrollados en lenguaje C\t[10%].\n\n")
+    printf("Archivo de código fuente:\t\"D\"\n");
     
-    letra = get_char("Ingresa la inicial de tu primer nombre: ");
-    numero_decimal = get_double("Ingresa un numero con punto decimal: ");
-    numero_entero = get_int("Ingresa un numero entero: ");
-    cadena_de_caracteres = get_string("Ingresa una cadena de caracteres (texto): ");
-
-
-    printf("La inicial que ingresaste es:\t%c\n",letra);
-    printf("El numero con punto decimal que ingresaste es:\t%f\n",numero_decimal);
-    printf("El numero entero que ingresaste es:\t%d\n",numero_entero);
-    printf("La cadena de caracteres (texto) que ingresaste es:\t%s",cadena_de_caracteres);
-
-    printf("\t\tEl programa ha \"finalizado\"\n\n")
 
     return EXIT_SUCCESS;
 
